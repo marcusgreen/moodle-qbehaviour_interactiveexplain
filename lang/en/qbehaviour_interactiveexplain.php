4c7f9aa77b87626444e84cd897ca272209d9bca1
marcusgreen <marcusavgreen@gmail.com>
2020-06-25 21:04:39 +0000

    string  variable fix

--- lang/en/qbehaviour_interactiveexplain.php
+++ lang/en/qbehaviour_interactiveexplain.php
@@ -26,6 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'Interactive with explanation';
 $string['privacy:metadata'] = 'The Interactive with explanation question behaviour plugin does not store any personal data.';
-$string['responsewithreason'] = 'Feedback with reason';
+$string['responsewithreason'] = '{$a->response} | Reason: {$a->explanation}';
+
 
 
